1acc8f41e58bd5e41d64b6c3c9d190f826f7a23e
Martin Di iorio <martin@coderteams.com>
2019-03-05 18:08:02 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
+import Results from "./Results";
+import Details from "./Details";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <Router>
+          <Results path="/" />
+          <Details path="/details/:id" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
---
+++ src/Details.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+class Details extends React.Component {
+  render() {
+    return <h1>hi lol</h1>;
+  }
+}
+
+export default Details;
